7529530330ff9703f69f157703ce7add94d39b71
Nick Papa <nick@ntersol.com>
2025-04-11 19:58:18 -0700

    Fix linux build script

--- publish-linux.sh
+++ publish-linux.sh
@@ -1,2 +1,2 @@
-dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:IncludeAllContentForSelfExtract=true /p:PublishTrimmed=true
-# cp "bin\Release\net8.0\linux-x64\publish\PlayNext.exe" "PlayNext.exe"
+dotnet publish PlayNext.csproj -r linux-x64 -c Release --self-contained true /p:PublishSingleFile=true
+cp "bin/Release/net8.0/linux-x64/publish/PlayNext" "PlayNext"
